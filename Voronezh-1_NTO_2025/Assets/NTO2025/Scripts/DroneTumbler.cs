d08fb79e28ffe2a5d00b6825a7669564edf83c2c
kplaaa <102736559+kplaaa@users.noreply.github.com>
2025-02-20 09:29:50 +0300

    Drone & scene

---
+++ Voronezh-1_NTO_2025/Assets/NTO2025/Scripts/DroneTumbler.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace NTO2025.Scripts
+{
+    public class DroneTumbler: MonoBehaviour
+    {
+        [SerializeField] private GameObject _droneCamera;
+         
+        private bool _isDrone = false;
+
+        private void Start()
+        {
+            Player.instance.hmdTransforms[0].GetComponent<Camera>().enabled = !_isDrone;
+            _droneCamera.SetActive(_isDrone);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                _isDrone = !_isDrone;
+                Player.instance.hmdTransforms[0].GetComponent<Camera>().enabled = !_isDrone;
+                _droneCamera.SetActive(_isDrone);
+            }
+        }
+    }
+}
